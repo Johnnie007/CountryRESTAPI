083273d3302ae62f3cf658910cd002244a78d21c
Johnnie007 <81275727+Johnnie007@users.noreply.github.com>
2021-08-21 10:52:11 -0400

    updated routes to be case insensitive

--- server.js
+++ server.js
@@ -19,10 +19,18 @@ app.route('/world',)
     res.send(data)
 })
 
+console.log(data[108].name.replace(' ', ''))
+
 //TODO:
 //figure out how to update data with spaces
 app.get('/world/:info',(req, res) =>{
-    console.log(data);
-    const country = data.filter(country => req.params.info=== country.name || req.params.info === country.currency || req.params.info === country.language || req.params.info === country.continent )
+    
+    const country = data.filter( country => { 
+    
+     const search = req.params.info;
+     //search.replaceAll(" ", "");
+       return( req.params.info.toLowerCase() === country.name.toLowerCase().replace(' ', '') || req.params.info === country.currency || req.params.info.toLowerCase() === country.language.toLowerCase() || req.params.info.toLowerCase() === country.continent.toLowerCase() || req.params.info.toLowerCase() === country.capital.toLowerCase().replace(' ', '') || req.params.info.toLowerCase() === country.currency.toLowerCase() || req.params.info.toLowerCase() === country.hemisphere.toLowerCase().replace(' ', ''))
+       
+    })
     res.send(country)
 });
