f4219c4e829622ff6284820fe96f2cbf4c650252
Johnnie007 <81275727+Johnnie007@users.noreply.github.com>
2021-08-21 13:05:34 -0400

    updated status code for routes

--- server.js
+++ server.js
@@ -17,8 +17,13 @@ app.get('/', (req, res) => {
     res.send("Welcome to the Country API! Created by: Johnnie Hicks.")
 });
 // route for all the countries info
-app.route('/world',)
+app.route('/:world',)
 .get((req, res) => {
+    if(req.params.world != 'world'){
+        return(res.statusCode = 404,
+            res.setHeader('Content-type', 'text/plain'),
+            res.end("Error"))
+    }
     res.send(data)
 })
 
@@ -35,7 +40,13 @@ app.get('/world/:info',(req, res) =>{
     //filter the data
     const country = data.filter( country => { 
        return( search === country.name.toLowerCase().replace(/\s/g, '') || search === country.currency || search === country.language.toLowerCase() || search === country.continent.toLowerCase() || search === country.capital.toLowerCase().replace(/\s/g, '') || search === country.hemisphere.toLowerCase().replace(/\s/g, ''))
-       
     })
+
+     //function for items that are not found
+     if(country.length === 0 ){
+        return( res.statusCode = 404,
+         res.setHeader('Content-type', 'text/plain'),
+         res.end("Error"))
+     }
     res.send(country)
 });
