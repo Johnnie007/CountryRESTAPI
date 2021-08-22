9f25e4d9c02da7fbe8e393085931ed0ef04ce82b
Johnnie007 <81275727+Johnnie007@users.noreply.github.com>
2021-08-21 21:03:22 -0400

    added static page

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+app.use(express.static('public'))
 const data = require("./data/country");
-const fs = require('fs');
-app.use(cors());
-app.use(express.json());
-
 
 //Server
 app.listen(7000,() => {
@@ -35,18 +31,18 @@ app.get('/world/:info',(req, res) =>{
     let search = req.params.info.toLowerCase();
     // statement for the different abbrevations of america
     if(search === 'usa' || search === 'us' || search === 'america'){
-        res.send(data[187])
-    }
+       return res.send(data[187])
+    } 
     //filter the data
     const country = data.filter( country => { 
-       return( search === country.name.toLowerCase().replace(/\s/g, '') || search === country.currency || search === country.language.toLowerCase() || search === country.continent.toLowerCase() || search === country.capital.toLowerCase().replace(/\s/g, '') || search === country.hemisphere.toLowerCase().replace(/\s/g, ''))
+       return( search === country.name.toLowerCase().replace(/\s/g, '') || search === country.currency.toLowerCase() || search === country.language.toLowerCase() || search === country.continent.toLowerCase() || search === country.capital.toLowerCase().replace(/\s/g, '') || search === country.hemisphere.toLowerCase().replace(/\s/g, ''))
     })
 
      //function for items that are not found
      if(country.length === 0 ){
         return( res.statusCode = 404,
          res.setHeader('Content-type', 'text/plain'),
-         res.end("Error"))
+         res.send("ERROR PLEASE CHECK URL"))
      }
     res.send(country)
 });
